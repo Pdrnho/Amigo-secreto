ef1f42f71b3f4adba32bea25dd2286ff9112655f
Pdrnho <pedro_camargo91@hotmail.com>
2025-02-04 00:32:49 -0300

    Inicio da pratica

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+ let amigos = []
+
+ //criar uma função que adiciona o nome anexado no array. Utilize document.getElementById ou document.querySelector
+ function adicionarAmigo() {
+
+ }
+
+
+ document.getElementById('amigo')
