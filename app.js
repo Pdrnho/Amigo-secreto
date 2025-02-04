75fe87e055278143b1e17f09a2919bb22a6b87e0
Pedro <pedro_camargo91@hotmail.com>
2025-02-04 12:11:13 -0300

    Programa está imputando na lista e alertando campo vazio

--- app.js
+++ app.js
@@ -1,8 +1,31 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
- let amigos = []
-
+    let listaDeNomes = [];
+    
  //criar uma função que adiciona o nome anexado no array. Utilize document.getElementById ou document.querySelector
- function adicionarAmigo() {
+
+    function limparNome () {
+        nomeDoAmigo = document.getElementById('amigo');
+        nomeDoAmigo.value = ''
+    }
+
+function adicionarAmigo() {
+    console.log('botão foi pressionado');
+    let nomeDoAmigo = document.getElementById('amigo').value;
+
+
+    //se não tiver nada digitado será alertado.
+    if (nomeDoAmigo === '') {
+        alert ('Coloque o nome de uma pessoa.');
+    } else {
+        listaDeNomes.push(nomeDoAmigo);
+        console.log()
+
+        limparNome();
+
+        console.log('nome incluido');
+    }
+
+    
 
  }
 
